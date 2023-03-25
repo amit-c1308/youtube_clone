f7f41c8f3537a455f58a5b1c88833ac814532f3e
Amit Choudhary <amit.choudhary@publicissapient.com>
2023-03-25 23:29:23 +0530

    youtube clone app created

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,63 @@
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail, marginTop }) => {
+  return (
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        width: { xs: "356px", md: "320px" },
+        height: "326px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "0 auto",
+        marginTop,
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: 180,
+              width: 180,
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}{" "}
+            <CheckCircle sx={{ color: "gray", ml: "5px", fontSize: "14px" }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}
+              &nbsp; Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
+};
+
+export default ChannelCard;
